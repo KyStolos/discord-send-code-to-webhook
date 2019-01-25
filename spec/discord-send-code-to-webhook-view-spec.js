e503e1b3ff2ad462b6b77eae793bec89c1c75afb
Boris Brogle <boris_brogle@jds.local>
2019-01-25 13:34:39 +0100

    First commit

---
+++ spec/discord-send-code-to-webhook-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import DiscordSendCodeToWebhookView from '../lib/discord-send-code-to-webhook-view';
+
+describe('DiscordSendCodeToWebhookView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
